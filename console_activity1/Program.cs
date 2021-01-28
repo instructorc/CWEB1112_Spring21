8b17c446a6de1dd952f2f7b8d8c060e5f3606fa7
C. Fulton <cfulton@duwnoody.edu>
2021-01-28 14:28:23 -0600

    adding console_activity 1

---
+++ console_activity1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace console_activity1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string favNum;
+            
+            Console.WriteLine("Enter your your favorite number ");
+            favNum = Console.ReadLine();
+            double favNumConverted = Double.Parse(favNum);
+            Console.WriteLine(favNum.GetType());
+            Console.WriteLine(favNumConverted.GetType());
+            Console.WriteLine("Yoru Favorite number is: {0}", favNumConverted);
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+    }
+}
