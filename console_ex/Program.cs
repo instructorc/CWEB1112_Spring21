71a5ad69b00af5e80381f4452d6701d021c07020
C. Fulton <cfulton@duwnoody.edu>
2021-01-28 13:27:45 -0600

    initial Commit for class repository

---
+++ console_ex/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace console_ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int FAVORITE_NUM = 32; //Integer stores a numeric value without decimals
+            string name = "Chris";
+            double salary = 1200.00;
+            bool isFound = true;
+            char favoriteLetter = 'A';
+      
+            Console.WriteLine($"The Salary is:\t {salary.ToString("c")}. \nMy favorite letter is:\t {favoriteLetter} ");
+         
+        }
+    }
+    class SomethingElse
+    {
+        
+    }
+}
