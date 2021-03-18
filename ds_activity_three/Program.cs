2e1a1fc8655d5621de87e723ae6ffe4b6352dac1
C. Fulton <cfulton@duwnoody.edu>
2021-03-18 14:26:26 -0500

    adding additional examples

---
+++ ds_activity_three/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ds_activity_three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DELARATIONS
+               //Declare array
+            string [] items = {"Nuts", "Fish", "Lentils", "Whole Grains", "Beans", "Olive Oil", "Eggs", "Yogurt"};
+            double [] prices = {.59, 2.00, .89, .99, .79, 5.25, 2.99, 1.25};
+            
+            //Declare Output message
+             const string OUTPUT = "Fish Found";
+
+             //Flag variable
+             bool isFound = false;
+
+            //initial input
+            int x = 0;
+            string item="";
+            double price =0;
+        
+            //looping structure
+            while(x < items.Length){
+                //decision making logic
+                if(items[x].ToLower() == "eggs".ToLower()){
+                    isFound = true;
+                    item = items[x];
+                    price = prices[x];
+                }
+                //increment array
+                x++;
+            }
+            
+            if(isFound){
+                Console.WriteLine($"You have found {item} for a price of {price.ToString("c")}");
+            }
+            else{
+                Console.WriteLine($"You have found {item} for a price of {price.ToString("c")}");
+            }
+            /**isFound = true ? Console.WriteLine($"You have found {item} for a price of {price.ToString("c")}") : Console.WriteLine($"You have found {item} for a price of {price.ToString("c")}");**/
+
+            //conclude program
+            Console.WriteLine("Program has ended");
+        }
+    }
+}
---
+++ ds_multiarray/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ds_multiarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DECLARATIONS
+            //2-dimensional Array
+            int [ , ] RENT_BY_FLOOR_AND_BDRMS = {
+                {350, 390, 435},
+                {400, 440, 480},
+                {475, 530, 575},
+                {600, 650, 700},
+                {1000, 1075, 1150}
+            };
+
+            int floorNumber, aptRoomCount;
+
+            double rent;
+
+            Console.WriteLine("Please input your desired apartment floor");
+            floorNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please input your desired room apartment room count");
+            aptRoomCount = Convert.ToInt32(Console.ReadLine());
+
+
+            rent = RENT_BY_FLOOR_AND_BDRMS[floorNumber, aptRoomCount];
+
+            Console.WriteLine($"Your rent is {rent.ToString("c")}");
+
+            //Target Center Seating Chart
+
+         
+        }
+    }
+}
